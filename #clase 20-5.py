32a2d50474dfb63e7db7fba08053b4fe447f9fe5
banana-Octopus <sar.quitral@duocuc.cl>
2026-05-27 17:51:17 -0400

    Add files via upload

---
+++ #clase 20-5.py
@@ -0,0 +1,16 @@
+#clase 20-5
+
+"""nombre="python"
+
+for letras in nombre:
+    print(letras)"""
+
+
+#while true
+
+while True:
+    edad=int(input("ingrese su edad"))
+    if edad>=0:
+        break
+
+
